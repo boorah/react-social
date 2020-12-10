24dc58ac764fd9c5405e94e1196dd2a1549746da
boorah <iamhimanshu808@gmail.com>
2020-12-10 15:39:41 +0530

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    Hello, React
+    <img src="" alt=""/>
+  </div>,
+  document.getElementById("root")
+);
